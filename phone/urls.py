f20043921a7a6d437e792a9b886fab58ffb6b61f
xiechunhui <xiechunhui@hisense.com>
2021-02-23 16:16:37 +0800

    xie:try3

--- phone/urls.py
+++ phone/urls.py
@@ -8,6 +8,7 @@ from .views import PhoneListView, PhoneInputView, data_phone_input
 app_name = 'phone'
 
 urlpatterns = [
+
     path('phone_borrow/', PhoneListView.as_view(), name='phone_borrow'),
     path('phone_input/',PhoneInputView.as_view(), name = 'phone_input'),
     path('data_phone_input/', data_phone_input, name = 'data_phone_input')
